d8c4630eb343ae8806ebd86e67075cacc5aab1a5
Chandi Subedi <csubedee@gmail.com>
2023-02-21 11:33:15 +0900

    Update excel-split.py
    Use explode fuction

--- excel-split.py
+++ excel-split.py
@@ -1,41 +1,14 @@
-import openpyxl
+import pandas as pd
 
-# Load the original Excel file and sheet
-workbook = openpyxl.load_workbook('original_file.xlsx')
-sheet = workbook.active
+# Read the original Excel file into a DataFrame
+df = pd.read_excel('original_file.xlsx')
 
-# Get the data from the sheet and create a list of tuples
-data = []
-for row in sheet.iter_rows(min_row=2, values_only=True):
-    name, age, nationality, sex = row
-    age = age.split(',') if isinstance(age, str) else ['']
-    nationality = nationality.split(',') if isinstance(nationality, str) else ['']
-    sex = sex.split(',') if isinstance(sex, str) else ['']
-    max_len = max(len(age), len(nationality), len(sex))
-    age += [''] * (max_len - len(age))
-    nationality += [''] * (max_len - len(nationality))
-    sex += [''] * (max_len - len(sex))
-    data.append(tuple(zip(name.split(','), age, nationality, sex)))
+# Explode the DataFrame
+exploded_df = df.explode('Name').explode('Age').explode('Nationality').explode('Sex')
 
-# Explode the data into a list of dictionaries
-exploded_data = []
-for item in data:
-    for i in range(len(item[0])):
-        row = {
-            'Name': item[0][i].strip(),
-            'Age': item[1][i].strip(),
-            'Nationality': item[2][i].strip(),
-            'Sex': item[3][i].strip()
-        }
-        if all(row.values()):
-            exploded_data.append(row)
+# Remove rows with any missing values
+exploded_df = exploded_df.dropna()
 
-# Write the exploded data to a new sheet
-new_workbook = openpyxl.Workbook()
-new_sheet = new_workbook.active
-new_sheet.append(['Name', 'Age', 'Nationality', 'Sex'])
-for item in exploded_data:
-    new_sheet.append([item['Name'], item['Age'], item['Nationality'], item['Sex']])
-
-# Save the new workbook to a new file
-new_workbook.save('new_file.xlsx')
+# Write the exploded data to a new sheet in a new Excel file
+with pd.ExcelWriter('new_file.xlsx') as writer:
+    exploded_df.to_excel(writer, index=False)
